9557a9429b992a3e7da7c7c3056752dc2526a33c
viracheva <virachevas@gmail.com>
2017-04-01 11:33:43 +0500

    Add project

---
+++ Origins2/app/src/main/java/com/example/as/origins/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.as.origins;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.as.origins.fragments.About;
+import com.example.as.origins.fragments.Account;
+import com.example.as.origins.fragments.Achievements;
+import com.example.as.origins.fragments.Finished;
+import com.example.as.origins.fragments.Help;
+import com.example.as.origins.fragments.Trails;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        displaySelectedScreen(R.id.content_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void displaySelectedScreen(int itemId){
+
+        Fragment fragment = null;
+
+        switch (itemId){
+            case R.id.trails:
+                fragment = new Trails();
+                break;
+            case R.id.finished:
+                fragment = new Finished();
+                break;
+            case R.id.achievements:
+                fragment = new Achievements();
+                break;
+            case R.id.account:
+                fragment = new Account();
+                break;
+            case R.id.about:
+                fragment = new About();
+                break;
+            case R.id.help:
+                fragment = new Help();
+                break;}
+
+            if (fragment != null){
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.content_frame, fragment);
+                ft.commit();
+            }
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+
+        }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        displaySelectedScreen(item.getItemId());
+        return true;
+    }
+}
